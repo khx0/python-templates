2bfe12f65512867428e0ee456bbe3241199d1d75
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-21 23:11:26 +0100

    added plain_assay_script.py

--- plain_assay_script.py
+++ plain_assay_script.py
@@ -4,8 +4,19 @@
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
 # date: 2020-02-21
-# file: plain_main.py
+# file: plain_assay_script.py
 ##########################################################################################
+
+import sys
+import datetime
+import os
+import numpy as np
+
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+RAWDIR = os.path.join(BASEDIR, 'raw')
+OUTDIR = os.path.join(BASEDIR, 'out')
  
 if __name__ == '__main__':
     
