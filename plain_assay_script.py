8d67530af3d41c32faabb82208fb8436eb207c82
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-29 20:56:26 +0100

    added makedirs command

--- plain_assay_script.py
+++ plain_assay_script.py
@@ -11,13 +11,17 @@ import sys
 import datetime
 import os
 import numpy as np
+import platform
 
 today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 RAWDIR = os.path.join(BASEDIR, 'raw')
 OUTDIR = os.path.join(BASEDIR, 'out')
- 
+
+os.makedirs(RAWDIR, exist_ok = True)
+os.makedirs(OUTDIR, exist_ok = True)
+
 if __name__ == '__main__':
     
-    pass
+    print(__file__, "running using python", platform.python_version())
